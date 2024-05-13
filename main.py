e4eecfbcb5350b35f9b9cd647c562ee09e559b78
Alex Kitchen <alexkitchen613@gmail.com>
2024-05-13 11:37:28 -0400

    Added log file

--- main.py
+++ main.py
@@ -1,30 +1,73 @@
 import requests
+import const
+import os
+from datetime import datetime
+
+debug = False
+
+def fetch_account():
+    log("Fetching account data")
+    get_puuid()
+    
+
+def main():
+    log(f"Logged in at {datetime.now()} with puuid {get_puuid()}")
+
+def log(message):
+    with open('log.txt', 'a') as file:
+        file.write(f"{datetime.now()} | {message} \n")
 
 def get_puuid():
+    if os.path.getsize('puuid.txt') == 0:
+        return None
     # Read puuid from file
     with open('puuid.txt', 'r') as file:
         puuid = file.read().strip()
+   
+    return puuid
 
 def set_puuid(puuid):
     # Write puuid to file
     with open('puuid.txt', 'w') as file:
         file.write(puuid)
 
-#3clips3#8715
-
-
-
 def puuid_from_username(username, tag):
-    # Get puuid from username and tag
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token":
-    }
+# Get puuid from username and tag
+    # get headers from const.py        
+    headers = const.headers
+    
+    # get puuid from username and tag
     response = requests.get(f'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{username}/{tag}', headers = headers)
+    
+    # handle error codes
     if response.status_code != 200:
         return response
+    
+    # return puuid
     return response.json()['puuid']
-print(puuid_from_username('3clips3', '8715'))
+
+#Test Case
+def test_case():
+    print(puuid_from_username(const.testUser, const.testTag))
+
+if __name__ == "__main__":
+    #make sure puuid.txt exists
+    if not os.path.exists('puuid.txt'):
+        with open('puuid.txt', 'w') as file:
+            pass
+
+    #run test case if debug is true
+    if debug:
+       test_case()
+    
+    #normal boot
+    else:
+        #Check for puuid
+        if get_puuid() is None:
+            username = input("Enter your username: ")
+            tag = input("Enter your tag: ")
+            set_puuid(puuid_from_username(username, tag))
+            log(f"Set puuid to {get_puuid()}")
+        else:
+            log(f"PUUID already set to {get_puuid()}")
+        main()
